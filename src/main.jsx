4eb70a8b0361d3c4d70262a2f509d9a5b817435b
Aditya Raj <adityaraj7320@gmail.com>
2025-04-02 22:58:19 +0530

    errors removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
 import PopUp from "./components/PopUp.jsx";
-import GraphPage from "./components/GraphPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<PopUp />} />
-        <Route path="/graph" element={<GraphPage />} />
-      </Routes>
-    </HashRouter>
+    <PopUp />
   </StrictMode>
 );
